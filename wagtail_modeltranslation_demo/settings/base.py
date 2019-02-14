8a68915e552cdf9bea0e00afbb5173a99e69aff1
Diogo Marques <diogomarques@Diogos-MacBook-Pro.local>
2019-02-14 14:26:01 +0000

    Setup

--- wagtail_modeltranslation_demo/settings/base.py
+++ wagtail_modeltranslation_demo/settings/base.py
@@ -169,3 +169,5 @@ WAGTAIL_SITE_NAME = "wagtail_modeltranslation_demo"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+WAGTAILMODELTRANSLATION_TRANSLATE_SLUGS = False
