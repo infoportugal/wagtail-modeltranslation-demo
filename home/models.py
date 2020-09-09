767f275c6948932e52fb54ea97a75b358103d46c
Diogo Marques <diogomarques@Diogos-MacBook-Pro.local>
2020-09-09 11:36:16 +0100

    WIP

--- home/models.py
+++ home/models.py
@@ -1,4 +1,7 @@
-from wagtail.admin.edit_handlers import FieldPanel
+from django.db import models
+from modelcluster.models import ClusterableModel
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
 
@@ -8,4 +11,24 @@ class HomePage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
+        InlinePanel("sub_models"),
     ]
+
+
+# class TopModel(ClusterableModel):
+#     panels = [
+#         InlinePanel("sub_models"),
+#     ]
+
+
+class SubModel(ClusterableModel):
+    top_model = ParentalKey(HomePage, related_name="sub_models", on_delete=models.CASCADE)
+
+    panels = [
+        InlinePanel("sub_sub_models"),
+    ]
+
+
+class SubSubModel(models.Model):
+    sub_model = ParentalKey(SubModel, on_delete=models.CASCADE, related_name="sub_sub_models")
+    body = RichTextField(blank=True)
--- home/translation.py
+++ home/translation.py
@@ -1,9 +1,13 @@
 from modeltranslation.decorators import register
 from modeltranslation.translator import TranslationOptions
 
-from .models import HomePage
+from .models import HomePage, SubSubModel
 
 
 @register(HomePage)
 class HomePageTR(TranslationOptions):
     pass
+
+@register(SubSubModel)
+class SubSubModelTR(TranslationOptions):
+    fields = ('body',)
