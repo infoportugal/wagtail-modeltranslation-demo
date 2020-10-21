f536e77989237522ad7a171dc296ac0fada88ba1
Diogo Marques <diogomarques@Diogos-MacBook-Pro.local>
2020-10-21 12:32:55 +0100

    WIP

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
@@ -9,3 +10,17 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
     ]
+
+
+class SkiResortPage(Page):
+    top_destination_first_block = models.ForeignKey(
+        "home.HomePage",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="ski_resort_pages_top_destination_first_block",
+    )
+
+class DestinationPage(Page):
+    heading_h1 = models.CharField(max_length=100, blank=True)
+    heading_h2 = models.CharField(max_length=100, blank=True)
--- home/translation.py
+++ home/translation.py
@@ -1,9 +1,22 @@
 from modeltranslation.decorators import register
 from modeltranslation.translator import TranslationOptions
 
-from .models import HomePage
+from .models import DestinationPage, HomePage, SkiResortPage
 
 
 @register(HomePage)
 class HomePageTR(TranslationOptions):
     pass
+
+
+@register(SkiResortPage)
+class SkiResortPageTR(TranslationOptions):
+    pass
+
+
+@register(DestinationPage)
+class DestinationPageTR(TranslationOptions):
+    fields = (
+        "heading_h1",
+        "heading_h2",
+    )
